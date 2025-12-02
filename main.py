ef089cb0ad6723dc4003cd6d4df49e6d27811f9f
EthanRooke <ant23qfu@uea.ac.uk>
2025-12-02 13:34:17 +0000

    Main made, installed mp hands

---
+++ main.py
@@ -0,0 +1,13 @@
+import cv2
+import mediapipe as mp
+
+# Initialize MediaPipe Hands
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=2,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
