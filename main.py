69f6315884ecebdd95983078e70f78b7b7873cd7
EthanRooke <ant23qfu@uea.ac.uk>
2025-12-02 14:23:01 +0000

    changes

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import cv2
 import mediapipe as mp
+import os
+import pandas as pd
 
-# Initialize MediaPipe Hands
+# Initialise mediapipe.hands
 mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
 hands = mp_hands.Hands(
-    static_image_mode=False,
-    max_num_hands=2,
+    static_image_mode=True,
+    max_num_hands=1,
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
 )
+
+# Path to your image folder
+image_folder = "~/Documents/Computer Science/Year 3/AI/AI_CW2/CW2_Temp_Dataset"  # folder path
+output_file = "Temp_Sorted_Data.csv"  # Output file name
+
+data_rows = []
+
+for filename in os.listdir(image_folder):
+    if filename.endswith(".jpg"):
+        image = cv2.imread(os.path.join(image_folder, filename))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
+
 
